b6ed7d1fcb0656752c3e84db8a864623c8dc1826
Patrick Moran <patrick_moran@mac.com>
2016-03-09 17:42:26 -0500

    patricks solution

---
+++ db/seed.rb
@@ -0,0 +1,10 @@
+
+
+Artist.create(name: "Vincent Van Gogh", nationality: "Dutch")
+Painting.create(title: "Starry Night", artist_id: 1)
+
+Artist.create(name: "Pablo Picasso", nationality: "Spanish")
+Painting.create(title: "Guernica", artist_id: 2)
+
+Artist.create(name: "Claude Money", nationality: "French")
+Painting.create(title: "Water Lilies", artist_id: 3)
