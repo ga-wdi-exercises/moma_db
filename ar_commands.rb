35724703edc732f2833408cd6f2963f3fd7489de
Clarissa Stark <clarissa.stark@gmail.com>
2016-03-09 18:22:51 -0500

    completed part 3

--- ar_commands.rb
+++ ar_commands.rb
@@ -6,26 +6,54 @@ require_relative "models/painting"
 
 # 1. Create a new Artist (Vincent Van Gogh) and save him to your database
 
+Artist.create(name: "Vincent Van Gogh", nationality: "Dutch")
+
 # 2. Create a new Painting (Starry Night) and save it to your database
+
+Painting.create(title:"Starry Night", artist_id: 1)
 
 # 3. Return all Artists
 
+Artist.all
+
 # 4. Return all Paintings
+
+Painting.all
 
 # 5. Return the artist with an id of 2
 
+Artist.find(2)
+
 # 6. Return the artist with a name of "Vincent Van Gogh"
+
+Artist.find_by(name:"Vincent Van Gogh")
 
 # 7. Return all the artists from Spain
 
+Artist.where(nationality: "Spanish")
+
 # 8. Return the painting with an id of 1
+
+Painting.find(1)
 
 # 9. Return the painting with a name of "Guernica"
 
+Painting.find_by(title: "Guernica")
+
 # 10. Return all the paintings that "Pablo Picasso" painted
+
+Painting.where(artist_id: 1)
 
 # 11. Return the artist of "Guernica"
 
+Painting.find_by(title: "Guernica").artist_id
+=> 2
+Artist.find(2)
+
 # 12. Change Vincent Van Gogh's nationality to USA
 
+Artist.find_by(name: "Vincent Van Gogh").update(nationality: "USA")
+
 # 13. Change "Starry Night" to be one of Pablo Picasso's paintings
+
+Painting.find_by(title: "Starry Night").update(artist_id: 2)
