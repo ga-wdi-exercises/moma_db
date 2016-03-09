1dcaac00e8f14b4e408548ad370df7542707fb84
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-03-09 13:58:28 -0500

    initial commit, finished

---
+++ app.rb
@@ -0,0 +1,10 @@
+require 'pry'
+require 'pg'
+require 'active_record'
+require_relative 'models/artist'
+require_relative 'db/connection'
+require_relative 'models/painting'
+
+
+binding.pry
+puts "done"
