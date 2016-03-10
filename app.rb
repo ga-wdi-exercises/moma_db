198ddc9321a208cf8da8238c98d3a1aecff30496
winishasmith <winishasmith@gmail.com>
2016-03-09 23:02:35 -0500

    completed parts 1 & 2

---
+++ app.rb
@@ -0,0 +1,10 @@
+require "pry"
+require "active_record"
+require "pg"
+
+require_relative "models/artist"
+require_relative "models/painting"
+require_relative "db/connection"
+
+binding.pry
+puts "fixes pry"
