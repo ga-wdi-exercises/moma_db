4974c8b5f4ebe99f3bf1a381995be3382e7bc4b2
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-03-09 23:58:29 -0500

    finished ar_commands

--- ar_commands.rb
+++ ar_commands.rb
@@ -15,17 +15,22 @@ Painting.all
 # 5. Return the artist with an id of 2
 Artist.find(2)
 # 6. Return the artist with a name of "Vincent Van Gogh"
+Artist.find_by(name: "Vincent VanGogh")
 
 # 7. Return all the artists from Spain
-
+Artist.find_by(nationality: "Spanish")
 # 8. Return the painting with an id of 1
-
+Painting.find(4)
 # 9. Return the painting with a name of "Guernica"
-
+Painting.find_by(name: "Guernica")
 # 10. Return all the paintings that "Pablo Picasso" painted
-
+Painting.where(artist_id: 5)
+# (pssst... it's id #5 for me because I had to reload my seed file because I can't (appartenly) spell Van Gogh)
 # 11. Return the artist of "Guernica"
-
+No idea on this one.
 # 12. Change Vincent Van Gogh's nationality to USA
-
+vanGogh = Artist.find_by(nationality: "Dutch")
+vanGogh.update( nationality: "USA")
 # 13. Change "Starry Night" to be one of Pablo Picasso's paintings
+starry_night = Painting.find_by(artist_id: 4)
+starry_night.update(artist_id: 5)
