be41a20ab5b89f26f7c069fc3ab2275558a67674
Keith Prifte <kprift@gmail.com>
2016-03-09 19:17:09 -0500

    initial commit

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'pry'
+require 'pg'
+require 'active_record'
+
+require_relative "db/connection"
+require_relative "models/artist"
+require_relative "models/painting"
+
+
+
+
+
+puts "end of app.rb file"
--- ar_commands.rb
+++ ar_commands.rb
@@ -5,27 +5,30 @@ require_relative "models/artist"
 require_relative "models/painting"
 
 # 1. Create a new Artist (Vincent Van Gogh) and save him to your database
-
+  # Artist.create(name: 'Vincent van Gogh', nationality: 'Dutch')
 # 2. Create a new Painting (Starry Night) and save it to your database
-
+  # Painting.create(title: 'Starry Nigths', artist_id:
 # 3. Return all Artists
-
+    # Arist.all
 # 4. Return all Paintings
-
+    # Painting.all
 # 5. Return the artist with an id of 2
-
+    # Artist.find(2)
 # 6. Return the artist with a name of "Vincent Van Gogh"
-
+    # Artist.find_by(name: "Vincent van Gogh")
 # 7. Return all the artists from Spain
-
+    # Artist.find_by(nationality: "Spain")
 # 8. Return the painting with an id of 1
-
+    # Painting(1)
 # 9. Return the painting with a name of "Guernica"
-
+    # Painting.find_by(title: "Guernica")
 # 10. Return all the paintings that "Pablo Picasso" painted
-
+  # vince = Artist.find(1)
+  # Painting.find(vince.id)
 # 11. Return the artist of "Guernica"
-
+  # guernica =Painting.find_by(title: "Guernica")
+  # Artist.find(guernica.artist_id
 # 12. Change Vincent Van Gogh's nationality to USA
-
+    # vince.update(nationality: "USA")
 # 13. Change "Starry Night" to be one of Pablo Picasso's paintings
+    # Painting.find(1).update(artist_id: 2)
