540ae2a4473a9f894096d3de398513cec6d393f5
Charles <charlesthwang@gmail.com>
2016-03-10 08:35:57 -0500

    charleshwang_solution

--- ar_commands.rb
+++ ar_commands.rb
@@ -22,7 +22,7 @@ require_relative "models/painting"
 
 # 5. Return the artist with an id of 2
 
-  Artist.find(2) # Note: When I loaded my seed.rb file, the artist ids were 13, 14, 15
+  Artist.find(2) # Note: When I reloaded my seed.rb file, the artist ids were 13, 14, 15
 
 # 6. Return the artist with a name of "Vincent Van Gogh"
 
@@ -34,7 +34,7 @@ require_relative "models/painting"
 
 # 8. Return the painting with an id of 1
 
-  Painting.find(1) # Note: When I loaded my seed.rb file, the painting ids were 4, 5, 6
+  Painting.find(1) # Note: When I reloaded my seed.rb file, the painting ids were 4, 5, 6
 
 # 9. Return the painting with a name of "Guernica"
 
@@ -42,10 +42,22 @@ require_relative "models/painting"
 
 # 10. Return all the paintings that "Pablo Picasso" painted
 
-  
+  pablo = Artist.find_by(name: "Pablo Picasso")
+  pablo.paintings
 
 # 11. Return the artist of "Guernica"
 
+  guernica = Painting.find_by(title: "Guernica")
+  guernica.artist
+
 # 12. Change Vincent Van Gogh's nationality to USA
 
+  vincent = Artist.where(name: "Vincent Van Gogh")
+  vincent.update(nationality: "USA")
+  # I pretty sure this is right but pry gave me: ArgumentError: wrong number of arguments (1 for 2)
+
 # 13. Change "Starry Night" to be one of Pablo Picasso's paintings
+
+  starrynight = Painting.where(title: "Starry Night")
+  starrynight.artist = "Pablo Picasso"
+  # NoMethodError: undefined method `artist=' for #<Painting::ActiveRecord_Relation:0x007fbb698d41f0>
