45a6cb89d2333db521e33ed980f55eb4c579ccdd
Sean Tay <seantay@Seans-MacBook-Pro-2.local>
2016-06-22 18:07:05 -0400

    Complete Submit

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS artists;
+DROP TABLE IF EXISTS paintings;
+
+CREATE TABLE artists (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  nationality VARCHAR(255)
+);
+
+CREATE TABLE paintings (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  artist_id INT
+);
---
+++ db/seed.sql
@@ -0,0 +1,14 @@
+TRUNCATE TABLE paintings CASCADE;
+TRUNCATE TABLE artists CASCADE;
+
+ALTER SEQUENCE paintings_id_seq RESTART WITH 1;
+ALTER SEQUENCE artists_id_seq RESTART WITH 1;
+
+INSERT INTO artists (name, nationality) VALUES ('Vincent van Gogh', 'Dutch');
+INSERT INTO paintings (title, artist_id) VALUES ('Starry Night', 1);
+
+INSERT INTO artists (name, nationality) VALUES ('Pablo Picasso', 'Spanish');
+INSERT INTO paintings (title, artist_id) VALUES ('Guernica', 2);
+
+INSERT INTO artists (name, nationality) VALUES ('Claude Money', 'French');
+INSERT INTO paintings (title, artist_id) VALUES ('Starry Night', 3);
