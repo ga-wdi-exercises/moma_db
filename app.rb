ee0a4fddbed1c15ac71a36f65d10b866858a9c0a
Adina Pollack <adinap16@gmail.com>
2016-06-22 19:51:00 -0400

    fixed app.rb

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 require "active_record"
 
-require_relative "connection"
-require_relative "../models/artist"
-require_relative "../models/painting"
+require_relative "db/connection"
+require_relative "models/artist"
+require_relative "models/painting"
 require "pry"
 
 binding.pry
